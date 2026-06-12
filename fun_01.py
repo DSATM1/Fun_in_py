c20864774de4240831b65aac8d7bde7819d666cc
Suraj <surajsppokale@gmail.com>
2026-06-12 20:24:09 +0530

    refactor fun_01.py to improve function definitions and add comments for clarity

--- fun_01.py
+++ fun_01.py
@@ -4,10 +4,20 @@
     print("Boy Married a Girl")
 marriage()"""
 
-"""def greet(name):
+"""def greet(name):  #parameters
     print(f"Hello {name}!")
 
-greet("Suraj")
+greet("Suraj")  #arguments
 greet("Mohan")
 greet("Ranganna")"""
 
+"""for i in range(1,11):
+    print(f"2X{i}={2*i}")
+ #   print(f"3X{i}={3*i}") XXXX #This will not work  XXXX"""
+
+"""def tables(n):  #default parameter n=10
+    for i in range(1,11):
+        print(f"{n} X {i} = {n*i}")
+tables(2)
+tables(4)
+tables(12)"""
