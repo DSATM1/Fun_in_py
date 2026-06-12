24e16ea4ea95ea8789f7911d67ec50e9af6f9f53
Suraj <surajsppokale@gmail.com>
2026-06-12 20:03:07 +0530

    add function definitions and greetings in fun_01.py

---
+++ fun_01.py
@@ -0,0 +1,13 @@
+#---Functions in Python----
+
+"""def marriage():
+    print("Boy Married a Girl")
+marriage()"""
+
+"""def greet(name):
+    print(f"Hello {name}!")
+
+greet("Suraj")
+greet("Mohan")
+greet("Ranganna")"""
+
