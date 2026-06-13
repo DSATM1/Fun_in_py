da4836a8a25e823c94ac07690f30b2390b169d8c
Suraj <surajsppokale@gmail.com>
2026-06-13 06:34:57 +0530

    add factorial function implementation in lamda.py

--- lamda.py
+++ lamda.py
@@ -22,3 +22,11 @@ print(mul(4))"""
 
 students.sort(key = lambda x : x["name"],reverse = False)
 print(students)"""
+
+"""def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(3))"""
+
