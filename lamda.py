ec75883f027258c3a38b57a5a2f88ff663b782b0
Suraj <surajsppokale@gmail.com>
2026-06-13 06:07:18 +0530

    add initial implementation of add and name functions in lamda.py

---
+++ lamda.py
@@ -0,0 +1,10 @@
+
+"""def add(*n):
+    return sum(n)
+print(add(20,35))"""
+
+"""def name(**hesaru):
+    for key,value in hesaru.items():
+        print(f"{key}:{value}")
+name(age=24,city="Sira",naam="Suraj".upper())"""
+
