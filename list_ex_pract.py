5fc5a1c3a415e73a113e5743c4fe9d08e2df40ac
Suraj <surajsppokale@gmail.com>
2026-06-13 11:01:07 +0530

    refactor list_ex_pract.py to include function for removing duplicates and return second largest element

--- list_ex_pract.py
+++ list_ex_pract.py
@@ -1,1 +1,14 @@
 #-----List Practice-------
+
+# lst = [23,45,9,12,78,32,21]
+# uniq = list(set(lst))
+# sorted_list = sorted(uniq)
+# if len(uniq) < 2:
+#     print(len(uniq))
+# print(uniq[2])
+
+def remove_duplicate(lst):
+    uniq = list(set(lst))
+    sorted_list = sorted(uniq)
+    return sorted_list[-2]
+print(remove_duplicate([23,45,9,12,78,32,21]))
