6b1fe73c336f6cb4bf393dfe5fe042e3fb8dffa5
Suraj <surajsppokale@gmail.com>
2026-06-13 14:15:20 +0530

    add two_point function to list_ex_pract.py for merging two sorted lists

--- list_ex_pract.py
+++ list_ex_pract.py
@@ -75,3 +75,24 @@ print(find_missing([1,2,4,5]))"""
     
     return missing
 print(find_missing([1,2,4,5]))"""
+
+
+"""def two_point(lst1, lst2):
+    merged=[]
+    i = 0
+    j = 0
+    while i < len(lst1) and i < len(lst2):
+        if lst1[i] < lst2[j]:
+            merged.append(lst1[i])
+            i += 1
+        else:
+            merged.append(lst2[j])
+            j += 1
+    while i < len(lst1):
+        merged.append(lst1[i])
+        i += 1
+    while j < len(lst2):
+        merged.append(lst2[j])
+        j += 1
+    return merged
+print(two_point([1,3,5],[2,4,6]))"""
