6811d9bb6fbaf33aca81f8debb2523f10a17a568
Suraj <surajsppokale@gmail.com>
2026-06-13 06:04:41 +0530

    add name function to print key-value pairs from keyword arguments in lamda.py

--- lamda.py
+++ lamda.py
@@ -3,3 +3,7 @@
     return sum(n)
 print(add(20,35))"""
 
+"""def name(**hesaru):
+    for key,value in hesaru.items():
+        print(f"{key}:{value}")
+name(age=24,city="Sira",naam="Suraj".upper())"""
