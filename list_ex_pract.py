39ea3b930a9731c6c167bc8fe911517989b9b92e
Suraj <surajsppokale@gmail.com>
2026-06-14 20:06:12 +0530

    fix formatting and add missing print statement for triangle pattern in list_ex_pract.py

--- list_ex_pract.py
+++ list_ex_pract.py
@@ -35,3 +35,10 @@ for i in range(1,n+1):
     for j in range(i,n+1):
         print("*",end= " ")
     print()"""
+
+
+"""n = 5
+for i in range(1,n+1):
+    for j in range(1, i+1):
+        print(i,end= " ")
+    print()"""
