be74f16d9204da53bd22d131c3c906cf67226646
Suraj <surajsppokale@gmail.com>
2026-06-14 20:02:01 +0530

    fix indentation and formatting for pyramid pattern printing in list_ex_pract.py

--- list_ex_pract.py
+++ list_ex_pract.py
@@ -29,3 +29,9 @@ for i in range(0,n):
     for j in range(0, i+1):
         print("*",end= " ")
     print()"""
+
+"""n = 5
+for i in range(1,n+1):
+    for j in range(i,n+1):
+        print("*",end= " ")
+    print()"""
